6159dc5fa9d7146e7b4bdf6eb9f8475195324869
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-11-17 05:17:22 +0000

    マイスタジオ部分をコンポーネント化

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { VueDraggable } from 'vue-draggable-plus'
 import { domToPng } from 'modern-screenshot'
 
-import Card from '@/models/card.ts'
-import CardImage from '@/components/CardImage.vue'
-import SerifFrame from '@/components/SerifFrame.vue'
+import MyStudio from '@/components/MyStudio.vue'
 
 const main = ref<HTMLElement | null>(null)
-const cardList = ref<Array<Card | undefined>>([
-  undefined,
-  new Card(1001101, '小日向美穂', 0, 0, 'bef9093335fbcbe9e92a41d2d68a206d'),
-  new Card(1000101, '島村卯月', 0, 0, '0dabb79ff64691111a0abae2ffed01ce'),
-  new Card(1001301, '五十嵐響子', 0, 0, 'fb3b173c49703071b4dbdd5ed424640c'),
-  undefined,
-])
 
 const downloadImage = async () => {
   const dataUrl = await domToPng(main.value!)
@@ -27,38 +17,8 @@ const downloadImage = async () => {
 
 <template>
   <main ref="main">
-    <SerifFrame
-      direction="top"
-      message="はじめまして、プロデューサーさん！ 島村卯月、17歳です。私、精一杯頑張りますから、一緒に夢叶えましょうね♪よろしくお願いしますっ！"
-    />
-    <VueDraggable v-model="cardList" tag="ul">
-      <li v-for="(card, index) in cardList" v-bind:key="index">
-        <CardImage v-bind:card="card" />
-      </li>
-    </VueDraggable>
-    <SerifFrame direction="bottom" message="今日も1日がんばります！" />
+    <MyStudio />
   </main>
 
   <button v-on:click="downloadImage">Download Image</button>
 </template>
-
-<style scoped>
-main {
-  width: 300px;
-  padding: 5px 10px;
-  background: #000000;
-}
-
-main > ul {
-  display: flex;
-  font-size: 0;
-  margin-top: 0;
-  padding-left: 1em;
-}
-
-main > ul > li {
-  margin: 0;
-  padding: 0;
-  cursor: pointer;
-}
-</style>
