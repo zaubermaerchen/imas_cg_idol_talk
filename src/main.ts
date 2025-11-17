772709bd02fba691ccbcc3b4f0a56b49c0b5fdee
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-11-17 06:17:36 +0000

    Vue Final Modal追加

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import './assets/main.css'
 import 'element-plus/dist/index.css'
+import 'vue-final-modal/style.css'
 
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+import { createVfm } from 'vue-final-modal'
 
 import App from './App.vue'
 import { dependencyProviderPlugin } from '@/plugins/dependencyProviderPlugin.ts'
 
 const app = createApp(App)
 app.use(ElementPlus)
+app.use(createVfm())
 app.use(dependencyProviderPlugin)
 app.mount('#app')
