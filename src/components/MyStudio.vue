f53a3ded5970ae077b5e53984b207e989de3cd54
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-11-17 11:13:05 +0000

    初期状態を空にする

--- src/components/MyStudio.vue
+++ src/components/MyStudio.vue
@@ -13,21 +13,8 @@ import { useTargetCard } from '@/composables/targetCard.ts'
 import { useTargetSerif } from '@/composables/targetSerif.ts'
 import { findCardByPosition } from '@/utils/card.ts'
 
-const cardList = ref<Array<Card | undefined>>([
-  undefined,
-  new Card(1001101, '小日向美穂', 0, 0, 'bef9093335fbcbe9e92a41d2d68a206d'),
-  new Card(1000101, '島村卯月', 0, 0, '0dabb79ff64691111a0abae2ffed01ce'),
-  new Card(1001301, '五十嵐響子', 0, 0, 'fb3b173c49703071b4dbdd5ed424640c'),
-  undefined,
-])
-const serifList = ref<Serif[]>([
-  new Serif(
-    true,
-    0,
-    'はじめまして、プロデューサーさん！ 島村卯月、17歳です。私、精一杯頑張りますから、一緒に夢叶えましょうね♪よろしくお願いしますっ！',
-  ),
-  new Serif(false, 0, '今日も1日がんばります！'),
-])
+const cardList = ref<Array<Card | undefined>>(Array.from({ length: 5 }))
+const serifList = ref<Serif[]>([new Serif(true), new Serif(false)])
 
 const isVisibleCardSelectorModal = ref(false)
 const { targetCardIndex, targetCard } = useTargetCard(cardList)
